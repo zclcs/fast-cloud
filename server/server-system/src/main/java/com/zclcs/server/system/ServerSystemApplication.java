710da50c35360a4f70cef094d97e77205f77e3a8
zclcs <2371219112@qq.com>
2021-08-15 22:41:08 +0800

    111111111

---
+++ server/server-system/src/main/java/com/zclcs/server/system/ServerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zclcs.server.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSystemApplication.class, args);
+    }
+
+}
