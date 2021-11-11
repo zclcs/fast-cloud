08d67730dfda480219f909b2c3bc3ce97d1bd881
zclcs <2371219112@qq.com>
2021-11-11 09:32:48 +0800

    字典服务启动刷新顺序变更 建库脚本，nacos脚本更新

--- server/server-dict/src/main/java/com/zclcs/server/dict/runner/StartedUpRunner.java
+++ server/server-dict/src/main/java/com/zclcs/server/dict/runner/StartedUpRunner.java
@@ -1,7 +1,11 @@
 package com.zclcs.server.dict.runner;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import com.zclcs.common.core.utils.BaseUtil;
+import com.zclcs.server.dict.provider.DictProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,15 +17,20 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class StartedUpRunner implements ApplicationRunner {
 
     private final ConfigurableApplicationContext context;
     private final Environment environment;
+    private final DictProvider dictProvider;
 
     @Override
     public void run(ApplicationArguments args) {
         if (context.isActive()) {
             BaseUtil.printSystemUpBanner(environment);
+            log.info("字典刷新事件启动：{}", DateUtil.date().toString(DatePattern.NORM_DATETIME_MS_PATTERN));
+            dictProvider.refreshAllDictTypeCache();
+            log.info("字典刷新事件启动完毕：{}", DateUtil.date().toString(DatePattern.NORM_DATETIME_MS_PATTERN));
         }
     }
 }
